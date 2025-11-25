1f8f3cfb15509cb85955b065e3cd5d4414522b87
tinka-ai <tinka.ai.srl@gmail.com>
2025-11-25 10:46:12 +0200

    Update LocalePageClient.tsx

--- components/LocalePageClient.tsx
+++ components/LocalePageClient.tsx
@@ -1,10 +1,9 @@
-'use client'
+"use client"
 
 import { useLocale } from "@/contexts/locale-context"
 
 export default function LocalePageClient({ children }: { children: (t: any) => React.ReactNode }) {
   const { t: T } = useLocale() as any
-
   const t = (path: string) =>
     path.split(".").reduce((acc: any, k: string) => acc?.[k], T) ?? path
 
